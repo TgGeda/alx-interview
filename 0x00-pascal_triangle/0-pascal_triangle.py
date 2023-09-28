b49b3fcd59c6e3f8dfad8f646136ee333e9add30
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-09-28 11:48:17 +0300

    It is some modification for 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-'''A module for working with Pascal's triangle.
-'''
-
-
-def pascal_triangle(n):
-    '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
-    triangle = []
-    if type(n) is not int or n <= 0:
-        return triangle
-    for i in range(n):
-        line = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                line.append(1)
-            elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
-    return triangle
-=======
-
 
 def pascal_triangle(n):
     """
@@ -45,4 +22,4 @@ def pascal_triangle(n):
 
 if __name__ == "__main__":
     print(pascal_triangle(5))
->>>>>>> a9655a69dfa8fdf589afbf3f0329d114ac716545
+
